acf7199fc33d711edde4426bcf0f806941b9b3ca
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-04-03 16:04:44 +0200

    update font style

--- dynamic_15/hooks.py
+++ dynamic_15/hooks.py
@@ -10,7 +10,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/dynamic_15/css/dynamic_15.css"
+app_include_css = "/assets/dynamic_15/css/dynamic_15.css"
 # app_include_js = "/assets/dynamic_15/js/dynamic_15.js"
 
 # include js, css files in header of web template
