677480ae4524d52aa99746fcc6e6ffd11f0e657d
fr3ooon1 <fero>
2024-09-22 12:34:05 +0300

    delete categories doctype to move to altibyan application

--- dynamic_15/tebian/doctype/categories/categories.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, dynamic business solutions and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Categories", {
-// 	refresh(frm) {
-
-// 	},
-// });
